8d0b931e4930c43e73552a3b38d76c228e0dbedd
Hosung.Kim <hosungkim5108@gmail.com>
2025-11-27 13:37:33 +0900

    initial

---
+++ CleanArchitectureMVI/CleanArchitectureMVI/CleanArchitectureMVIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureMVIApp.swift
+//  CleanArchitectureMVI
+//
+//  Created by 김호성 on 2025.11.27.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureMVIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
